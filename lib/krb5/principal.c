1f57b271506d95c5babd968a747cbc4fd9fa221d
Santosh Kumar Pradhan <spradhan@redhat.com>
2014-02-21 09:48:31 +0530

    heimdal: Use krb5_free_default_realm() for free()
    The resource allocated by krb5_default_default_realm() should be
    free()'d by krb5_free_default_realm() instead of plain free()
    for better readability.
    
    Signed-off-by: Santosh Kumar Pradhan <spradhan@redhat.com>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- lib/krb5/principal.c
+++ lib/krb5/principal.c
@@ -357,7 +357,7 @@ exit:
 	free(comp[--n]);
     }
     free(comp);
-    free(realm);
+    krb5_free_default_realm(context, realm);
     free(s);
     return ret;
 }
@@ -447,7 +447,7 @@ unparse_name_fixed(krb5_context context,
 	    return ret;
 	if(strcmp(princ_realm(principal), r) != 0)
 	    short_form = 0;
-	free(r);
+	krb5_free_default_realm(context, r);
     }
     if(!short_form && !no_realm) {
 	add_char(name, idx, len, '@');
@@ -726,7 +726,7 @@ krb5_make_principal(krb5_context context,
     ret = krb5_build_principal_va(context, principal, strlen(realm), realm, ap);
     va_end(ap);
     if(r)
-	free(r);
+	krb5_free_default_realm(context, r);
     return ret;
 }
 
